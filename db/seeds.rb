f30a6026a2dc12a5761463d764aa3c01aa01de34
soartec-lab <info@soartec-lab.work>
2020-04-25 20:00:01 +0900

    power seed data add

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ user = User.find(1)
 
 BlockUser.create!(Array.new(10) { |i|  { user: user } })
 Article.create!(
-  Array.new(10000) do |i|
+  Array.new(10) do |i|
     {
       user: user,
       title: "title#{i}",
@@ -12,11 +12,11 @@ Article.create!(
     }
   end
 )
-SkillCategory.create!( Array.new(100) { |i| {name: "skill_category_#{i}" } })
+SkillCategory.create!( Array.new(10) { |i| {name: "skill_category_#{i}" } })
 
 skill_category = SkillCategory.find(1)
 Skill.create!(
-  Array.new(10000) do | i|
+  Array.new(30000) do | i|
     { user: user, skill_category: skill_category,name: "skill#{i}" }
   end
 )
