70f4806890d00c306ef19fb36b2168644a80515f
soartec-lab <info@soartec-lab.work>
2020-04-25 13:06:01 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,28 @@
 User.create!(Array.new(10) { |i|  { name: "user_#{1}"} })
-BlockUser.create!(Array.new(10) { |i|  { user_id: i } })
+
+user = User.find(1)
+
+BlockUser.create!(Array.new(10) { |i|  { user: user } })
 Article.create!(
   Array.new(10000) do |i|
     {
-      user_id: i,
-      title: "title#{i}"},
+      user: user,
+      title: "title#{i}",
       description: "description#{i}"
     }
   end
 )
+Skill.create!(Array.new(100) { |i|  { user: user, name: "skill#{i}" } })
+Category.create!(Array.new(100) {  |i|  { name: "skill#{i}" } })
 
+skill = Skill.find(1)
+category = Category.find(1)
+
+SkillCategory.create(
+  Array.new(100) do |i|
+    {
+      skill: skill,
+      category: category
+    }
+  end
+)
