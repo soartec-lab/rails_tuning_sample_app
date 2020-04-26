bc27d5b40d2a1433bbc5a7610dd23825d4b65ec4
soartec-lab <info@soartec-lab.work>
2020-04-26 21:34:18 +0900

    Add skill_category pattern

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,11 +24,18 @@ Tag.all.each { |tag| ArticleTag.create!(article: article, tag: tag) }
 
 SkillCategory.create!( Array.new(10) { |i| {name: "skill_category_#{i}" } })
 
-skill_category = SkillCategory.find(1)
+skill_category_1 = SkillCategory.find(1)
+skill_category_2 = SkillCategory.find(2)
 Skill.create!(
-  # Array.new(30000) do | i|
-  Array.new(10
-    ) do | i|
-    { user: user, skill_category: skill_category,name: "skill#{i}" }
+  # Array.new(30000) do |i|
+  Array.new(10) do |i|
+    { user: user, skill_category: skill_category_1,name: "skill_1_#{i}" }
+  end
+)
+Skill.create!(
+  # Array.new(30000) do |i|
+  Array.new(10) do |i|
+
+    { user: user, skill_category: skill_category_2,name: "skill_1_#{i}" }
   end
 )
