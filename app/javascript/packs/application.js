3ed52dc5a9bc6e89333f752182f306b48d283bac
soartec-lab <info@soartec-lab.work>
2020-04-27 23:09:55 +0900

    Add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import fontawesome from '@fortawesome/fontawesome'
+import faSolid from '@fortawesome/fontawesome-free-solid'
+import faRegular from '@fortawesome/fontawesome-free-regular'
+import faBrands from '@fortawesome/fontawesome-free-brands'
 
+fontawesome.library.add(faSolid, faRegular, faBrands)
+
+fontawesome.dom.i2svg()
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
