69dfa914d520fdd16d9294bedb0240a1743a9403
soartec-lab <info@soartec-lab.work>
2020-04-29 00:57:52 +0900

    Use insert_all method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,74 @@
 introduction = "Hello, World" * 10
-User.create!(
+User.insert_all(
   Array.new(10) do |i|
-    { name: "user_#{1}", introduction: introduction}
+    {
+      name: "user_#{1}",
+      introduction: introduction,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+    }
   end
 )
 
 user = User.find(1)
 
-BlockUser.create!(Array.new(10) { |i|  { user: user } })
-
-description = "Hello, World" * 8
-Article.create!(
-  Array.new(100) do |i|
+BlockUser.insert_all(
+  Array.new(10) do |i|
     {
-      user: user,
-      title: "title#{i}",
-      description: description
+      user_id: user.id,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
     }
   end
 )
-Tag.create!( Array.new(1000) { |i| {name: "tag_#{i}" } })
+
+description = "Hello, World" * 8
+Article.insert_all(
+  Array.new(100) do |i|
+    {
+      user_id: user.id,
+      title: "title#{i}",
+      description: description,
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+    }
+  end
+)
+Tag.insert_all(
+  Array.new(1000) do |i|
+    {
+      name: "tag_#{i}",
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+    }
+  end
+)
 
 Article.all.each do |article| 
-  Tag.all.sample(30).each { |tag| ArticleTag.create!(article: article, tag: tag) }
+  Tag.all.sample(30).each do |tag|
+    ArticleTag.create!(article_id: article.id, tag_id: tag.id)
+  end
 end
 
-SkillCategory.create!( Array.new(1000) { |i| {name: "skill_category_#{i}" } })
-Skill.create!(Array.new(50000) { |i| { user: user, name: "skill_1_#{i}" } })
+SkillCategory.insert_all(
+  Array.new(10000) do |i|
+    {
+      name: "skill_category_#{i}",
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+    }
+  end
+)
+Skill.insert_all(
+  Array.new(200000) do |i|
+    {
+      user_id: user.id,
+      name: "skill_1_#{i}",
+      created_at: Time.zone.now,
+      updated_at: Time.zone.now
+    }
+  end
+)
 
 SkillCategory.all.sample(10).each do |skill_category|
   Skill.all.sample(5).each do |skill|
