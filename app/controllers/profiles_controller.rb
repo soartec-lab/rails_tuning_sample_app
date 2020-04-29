ac64edfa74f29de79ca7a4f1276c5b9fff2b8dbb
soartec-lab <info@soartec-lab.work>
2020-04-29 19:46:37 +0900

    sample tuning codes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,34 @@
 class ProfilesController < ApplicationController
   def index
     @user = User.find(1)
-    @skill_categories = @user.skills.map(&:skill_category).uniq
-    @articles = @user.articles
+    # @skill_categories = @user.skills.map(&:skill_category).uniq.compact
+    # 4869ms
+    # @skill_categories = @user.skills.eager_load(:skill_category).map(&:skill_category).uniq.compact
+    # 6331ms
+    # @skill_categories = @user.skills.joins(:skill_category).map(&:skill_category).uniq.compact
+    # @skill_categories = []
+    @skill_categories = @user.skills.preload(:skill_category).map(&:skill_category).uniq.compact
+    # 4297ms
+
+    # @articles = @user.articles
+    # 4297ms
+    # @articles = @user.articles.preload(:tags)
+    # 4369ms
+    @articles = @user.articles.eager_load(:tags)
+    # 4241ms
   end
 end
+
+# @user = User.find(1)
+# Benchmark.realtime do
+#   # @skill_categories = @user.skills.eager_load(:skill_category).map(&:skill_category).uniq.compact
+#   @skill_categories = @user.skills.preload(:skill_category).map(&:skill_category).uniq.compact
+#   @skill_categories = @user.skills.joins(:skill_category).map(&:skill_category).uniq.compact
+# end
+
+# @skill_categories.each do |category|
+#   category.name
+#   @user.skills.where(skill_category: category).each do |skill|
+#     skill.name
+#   end
+# end
