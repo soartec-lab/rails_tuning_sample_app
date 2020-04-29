5f8de946211f7bab08668c94d093c5971ec214e6
soartec-lab <info@soartec-lab.work>
2020-04-30 01:31:17 +0900

    Delete model BlockUser

--- db/migrate/20200423130547_create_block_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateBlockUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :block_users do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
