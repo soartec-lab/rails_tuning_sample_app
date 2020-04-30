a3517bdaba0a88a85c2915db43ddf45bf7b87e10
soartec-lab <info@soartec-lab.work>
2020-04-30 23:22:12 +0900

    Add user validation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,17 @@
 class ProfilesController < ApplicationController
-  def index
+   def index
     @user = User.find(1)
+    raise Forbidden un less user_safe?
+
     @skill_categories = @user.skills.map(&:skill_category).uniq
     @articles = @user.articles
+  end
+
+  private
+
+  def user_safe?
+    @user.user_cautions.all? do |user_caution|
+      Time.zone.now > user_caution.caution_freeze.end_time
+    end
   end
 end
