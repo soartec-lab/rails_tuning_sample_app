00ef531d37905908ae1a8b3bd8bd223203d71733
soartec-lab <info@soartec-lab.work>
2020-04-30 23:21:34 +0900

    Fix ProfileController to symple

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,41 +1,7 @@
 class ProfilesController < ApplicationController
   def index
-    @user = User.find(1)  
-    @user.user_cautions.all? do |user_caution|
-      Time.zone.now > user_caution.caution_freeze.end_time
-    end
-
-    # @user.user_cautions.joins(:caution_freeze).
-    #   where("caution_freezes.end_time > ?", Time.zone.now).present?
-
-    # @skill_categories = @user.skills.map(&:skill_category).uniq.compact
-    # 4869ms
-    # @skill_categories = @user.skills.eager_load(:skill_category).map(&:skill_category).uniq.compact
-    # 6331ms
-    # @skill_categories = @user.skills.joins(:skill_category).map(&:skill_category).uniq.compact
-    # @skill_categories = []
-    @skill_categories = @user.skills.preload(:skill_category).map(&:skill_category).uniq.compact
-    # 4297ms
-
-    # @articles = @user.articles
-    # 4297ms
-    # @articles = @user.articles.preload(:tags)
-    # 4369ms
-    @articles = @user.articles.eager_load(:tags)
-    # 4241ms
+    @user = User.find(1)
+    @skill_categories = @user.skills.map(&:skill_category).uniq
+    @articles = @user.articles
   end
 end
-
-# @user = User.find(1)
-# Benchmark.realtime do
-#   # @skill_categories = @user.skills.eager_load(:skill_category).map(&:skill_category).uniq.compact
-#   @skill_categories = @user.skills.preload(:skill_category).map(&:skill_category).uniq.compact
-#   @skill_categories = @user.skills.joins(:skill_category).map(&:skill_category).uniq.compact
-# end
-
-# @skill_categories.each do |category|
-#   category.name
-#   @user.skills.where(skill_category: category).each do |skill|
-#     skill.name
-#   end
-# end
