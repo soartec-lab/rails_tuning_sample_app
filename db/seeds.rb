e97f1de468324a14104df6ff7043ad132328b326
soartec-lab <info@soartec-lab.work>
2020-04-30 23:04:41 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.insert_all(
 )
 
 user = User.find(1)
-user_caution_size = 5000
+user_caution_size = 3000
 UserCaution.insert_all(
   Array.new(user_caution_size) do |i|
     {
@@ -35,11 +35,10 @@ CautionFreeze.insert_all(
   end
 ) 
 
-article_size = 100
+article_size = 700
 description = "Hello, World" * 8
 Article.insert_all(
   Array.new(article_size) do |i|
-  # Array.new(10000) do |i|
     {
       user_id: user.id,
       title: "title#{i}",
@@ -50,7 +49,7 @@ Article.insert_all(
   end
 )
 
-tag_size = 10000
+tag_size = 5000
 Tag.insert_all(
   Array.new(tag_size) do |i|
     {
@@ -62,7 +61,7 @@ Tag.insert_all(
 )
 
 ArticleTag.insert_all(
-  Array.new(1000) do |i|
+  Array.new(4000) do |i|
     {
       article_id: Array(1..article_size).sample,
       tag_id: Array(1..tag_size).sample,
@@ -72,7 +71,7 @@ ArticleTag.insert_all(
   end
 )
 
-skill_category_size = 1000
+skill_category_size = 500
 SkillCategory.insert_all(
   # Array.new(10000) do |i|
   Array.new(skill_category_size) do |i|
@@ -85,7 +84,7 @@ SkillCategory.insert_all(
 )
 
 Skill.insert_all(
-  Array.new(10000) do |i|
+  Array.new(3000) do |i|
     {
       user_id: user.id,
       skill_category_id: Array(1..skill_category_size).sample,
@@ -95,3 +94,6 @@ Skill.insert_all(
     }
   end
 )
+
+# データバランス考える
+# rails db:migrate:reset && time rails db:seed
